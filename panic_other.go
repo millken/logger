8155f472930288ed2ab0ff0c614cbd092b7865db
millken <millken@gmail.com>
2020-07-25 11:19:52 +0800

    Update panic_other.go

--- panic_other.go
+++ panic_other.go
@@ -1,10 +1,11 @@
-// +build !windows
+// +build darwin dragonfly freebsd linux nacl netbsd openbsd
 
 package logger
 
 import (
 	"os"
-	"syscall"
+	
+	"golang.org/x/sys/unix"
 )
 
 // PanicFile Can be used by other packages.
@@ -12,13 +13,13 @@ import (
 var PanicFile *os.File
 
 func InitPanicFile(fileLocation string) error {
-	file, err := os.OpenFile(fileLocation, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+	file, err := os.OpenFile(fileLocation,os.O_WRONLY | os.O_CREATE | os.O_SYNC, 0644)
 	PanicFile = file
 	if err != nil {
 		println(err)
 		return err
 	}
-	if err = syscall.Dup2(int(file.Fd()), int(os.Stderr.Fd())); err != nil {
+	if err = unix.Dup2(int(file.Fd()), int(os.Stderr.Fd())); err != nil {
 		return err
 	}
 	return nil
